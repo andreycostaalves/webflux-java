9b6c7203795358f557073d5f01f41b2e0504c7e3
Andrey Alves <Andreyalvesgit@gmail.com>
2023-04-10 17:36:24 -0300

    add exception handle to email duplicated

---
+++ src/main/java/br/com/example/demo/controller/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,40 @@
+package br.com.example.demo.controller.exceptions;
+
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    ResponseEntity<Mono<StandardError>> duplicateKeyException(
+            DuplicateKeyException ex, ServerHttpRequest request
+    ){
+        return ResponseEntity.badRequest()
+                .body(Mono.just(
+                        StandardError.builder()
+                                .timestamp(LocalDateTime.now())
+                                .status(BAD_REQUEST.value())
+                                .error(BAD_REQUEST.getReasonPhrase())
+                                .message(verifyDupKey(ex.getMessage()))
+                                .path(request.getPath().toString())
+                                .build()
+                ));
+    }
+
+    private String verifyDupKey(String message){
+        if(message.contains("email dup key")){
+            return "E-mail already registered.";
+        }
+        return "Dup Key Exception.";
+    }
+
+}
