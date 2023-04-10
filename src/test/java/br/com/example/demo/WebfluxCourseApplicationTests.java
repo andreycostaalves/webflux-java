07ae593a15e3ef14d84e90551b8b3991541b8551
Andrey Alves <Andreyalvesgit@gmail.com>
2023-04-10 15:22:19 -0300

    Iniciando projeto webflux

---
+++ src/test/java/br/com/example/demo/WebfluxCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
