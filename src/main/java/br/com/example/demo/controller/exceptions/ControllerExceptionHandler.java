6598a9064973931acc65c82b60707e19f3961f20
Andrey Alves <Andreyalvesgit@gmail.com>
2023-04-10 18:08:17 -0300

    add exception handler validation error

--- src/main/java/br/com/example/demo/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/br/com/example/demo/controller/exceptions/ControllerExceptionHandler.java
@@ -3,12 +3,13 @@ package br.com.example.demo.controller.exceptions;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
-import java.time.LocalDateTime;
-
+import static java.time.LocalDateTime.now;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @ControllerAdvice
@@ -16,12 +17,13 @@ public class ControllerExceptionHandler {
 
     @ExceptionHandler(DuplicateKeyException.class)
     ResponseEntity<Mono<StandardError>> duplicateKeyException(
-            DuplicateKeyException ex, ServerHttpRequest request
+            DuplicateKeyException ex,
+            ServerHttpRequest request
     ){
         return ResponseEntity.badRequest()
                 .body(Mono.just(
                         StandardError.builder()
-                                .timestamp(LocalDateTime.now())
+                                .timestamp(now())
                                 .status(BAD_REQUEST.value())
                                 .error(BAD_REQUEST.getReasonPhrase())
                                 .message(verifyDupKey(ex.getMessage()))
@@ -29,6 +31,22 @@ public class ControllerExceptionHandler {
                                 .build()
                 ));
     }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<Mono<ValidationError>> validationError(
+            WebExchangeBindException ex,
+            ServerHttpRequest request
+    ){
+        ValidationError error = new ValidationError(
+                now(), request.getPath().toString(), BAD_REQUEST.value(),
+                "validation Error", "Error on validation attributes"
+        );
+        for (FieldError x : ex.getBindingResult().getFieldErrors()){
+            error.addError(x.getField(), x.getDefaultMessage());
+        }
+        return ResponseEntity.status(BAD_REQUEST).body(Mono.just(error));
+    }
+
 
     private String verifyDupKey(String message){
         if(message.contains("email dup key")){
--- src/main/java/br/com/example/demo/model/request/UserRequest.java
+++ src/main/java/br/com/example/demo/model/request/UserRequest.java
@@ -1,7 +1,17 @@
 package br.com.example.demo.model.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserRequest(
+        @Size(min = 3, max = 50, message = "Must be between 3 and 50 characters")
+        @NotBlank(message = "Must not be null or empty")
         String name,
+        @Email(message = "Invalid email")
+        @NotBlank(message = "Must not be null or empty")
         String email,
+        @Size(min = 3, max = 20, message = "Must be between 3 and 50 characters")
+        @NotBlank(message = "Must not be null or empty")
         String password
 ) { }
