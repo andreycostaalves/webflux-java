01107ced24c7b7230fcabcb1045b42c67c4a10e7
Andrey Alves <Andreyalvesgit@gmail.com>
2023-04-19 11:08:23 -0300

    TEST - unit test to findAll to users

--- src/test/java/br/com/example/demo/service/UserServiceTest.java
+++ src/test/java/br/com/example/demo/service/UserServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -50,7 +51,7 @@ class UserServiceTest {
 
     @Test
     void testFindById(){
-        when(repository.findById(anyString())).thenReturn(Mono.just(User.builder().id("123").build()));
+        when(repository.findById(anyString())).thenReturn(Mono.just(User.builder().build()));
 
         Mono<User> result = service.findById("123");
 
@@ -59,5 +60,18 @@ class UserServiceTest {
                 .expectComplete()
                 .verify();
         Mockito.verify(repository, times(1)).findById(anyString());
+    }
+
+    @Test
+    void testFindAll(){
+        when(repository.findAll()).thenReturn(Flux.just(User.builder().build()));
+
+        Flux<User> result = service.findAll();
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass()== User.class)
+                .expectComplete()
+                .verify();
+        Mockito.verify(repository, times(1)).findAll();
     }
 }
