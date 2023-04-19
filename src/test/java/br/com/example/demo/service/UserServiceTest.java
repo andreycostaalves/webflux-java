6757cb91638a618b78066814eab488ad79ad23f1
Andrey Alves <Andreyalvesgit@gmail.com>
2023-04-19 11:02:47 -0300

    TEST - unit test to findById to users

--- src/test/java/br/com/example/demo/service/UserServiceTest.java
+++ src/test/java/br/com/example/demo/service/UserServiceTest.java
@@ -8,13 +8,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import java.util.Objects;
-
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -40,9 +41,23 @@ class UserServiceTest {
         Mono<User> result = service.save(request);
 
         StepVerifier.create(result)
-                .expectNextMatches(Objects::nonNull)
+                .expectNextMatches(user -> user.getClass()== User.class)
                 .expectComplete()
                 .verify();
+        Mockito.verify(repository, times(1)).save(any(User.class));
 
+    }
+
+    @Test
+    void testFindById(){
+        when(repository.findById(anyString())).thenReturn(Mono.just(User.builder().id("123").build()));
+
+        Mono<User> result = service.findById("123");
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass()== User.class)
+                .expectComplete()
+                .verify();
+        Mockito.verify(repository, times(1)).findById(anyString());
     }
 }
