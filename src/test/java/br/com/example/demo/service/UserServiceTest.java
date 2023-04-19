acb6e7f60633d84e2a3f5b47b87b0965f56c4c0b
Andrey Alves <Andreyalvesgit@gmail.com>
2023-04-18 23:33:32 -0300

    TEST - add unit test to save user service

---
+++ src/test/java/br/com/example/demo/service/UserServiceTest.java
@@ -0,0 +1,48 @@
+package br.com.example.demo.service;
+
+import br.com.example.demo.entity.User;
+import br.com.example.demo.mapper.UserMapper;
+import br.com.example.demo.model.request.UserRequest;
+import br.com.example.demo.repositories.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Objects;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository repository;
+
+    @Mock
+    private UserMapper mapper;
+
+    @InjectMocks
+    private UserService service;
+
+
+    @Test
+    void save() {
+        UserRequest request = new UserRequest("andrey","andrey.alves@gmail.com", "123");
+        User entity = User.builder().build();
+        when(mapper.toEntity(any(UserRequest.class))).thenReturn(entity);
+        when(repository.save(any(User.class))).thenReturn(Mono.just(User.builder().build()));
+
+        Mono<User> result = service.save(request);
+
+        StepVerifier.create(result)
+                .expectNextMatches(Objects::nonNull)
+                .expectComplete()
+                .verify();
+
+    }
+}
